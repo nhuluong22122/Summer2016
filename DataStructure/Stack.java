8417c214bfd1e6b72fa3cdea1955eb73c5075645
Nhu Luong <nhuluong2212@gmail.com>
2016-08-04 13:41:22 -0700

    Add files via upload

--- DataStructure/Stack.java
+++ DataStructure/Stack.java
@@ -2,37 +2,33 @@ package datastructure;
 
 import java.util.LinkedList;
 
-public class Queue<T>{
-	LinkedList<T> list;
-	
-	/**
-	 * Constructor
-	 */
-	public Queue(){
+public class Stack<T>{//Generic T stands for Type
+	LinkedList<T> list; // Create the linkedlist
+
+	public Stack(){
 		list = new LinkedList<T>();
 	}
-	//O(n)
-	public void add(T n){
-		list.addLast(n);
+	//O(1)
+	public void add(T n){ // push 
+		list.addFirst(n);
 	}
 	//O(1)
-	public T remove(){
+	public T remove(){ // pop
 		return list.removeFirst();
 	}
 	//O(1)
 	public T peek(){
 		return list.peekFirst();
 	}
-	//Because size() of arraylist has O(1)
+	//O(1)
 	public boolean isEmpty(){
-		return list.isEmpty(); // or return list.size() == 0;
+		return list.isEmpty();
 	}
 	//O(1)
 	public int size(){
 		return list.size();
 	}
-	
-	//O(n) because we need to loop through the for loop
+	//O(n)
 	public void print(){
 		for(int i = 0; i < list.size(); i++){
 			 System.out.print(list.get(i) + " ");
